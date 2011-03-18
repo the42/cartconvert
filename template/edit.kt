d8a74637b4f018e0523e580a18f716bdff7eae14
Johann Höchtl <johann.hoechtl@gmail.com>
2011-03-18 22:38:19 +0100

    initial import of two new examples

---
+++ template/edit.kt
@@ -0,0 +1,11 @@
+<form action='/' method='post'>
+    <div>
+        <input name='xcoord' value='$xcoord'>
+        <input name='ycoord' value='$ycoord'>
+        <!-- <textarea name='body'>$body</textarea> -->
+    </div>
+    <div id='Actions'>
+        <input type='submit' value='Cancel'>
+        <input type='submit' name='submit' value='Save'>
+    </div>
+</form>
---
+++ template/layout.kt
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN'>
+<html>
+    <head>
+        <meta http-equiv='Content-type' content='text/html; charset=utf-8'>
+        <link href='/style.css' type='text/css' rel='stylesheet'>
+        <title>Geocord Conversion</title>
+    </head>
+    <body>
+        <div id='Container0'>
+            <div id='Container1'>
+                <div id='Edit'>$Edit.Render(edit)</div>
+            </div>
+        </div>
+    </body>
+</html>
