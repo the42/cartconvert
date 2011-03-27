3c19e3ab69754bf2c42cc624a14f26fa458a7c69
Johann Höchtl <johann.hoechtl@gmail.com>
2011-03-27 03:44:26 +0200

    Rewrite for go http server and template package

--- examples/cartconvserv/cartconvserv.go
+++ examples/cartconvserv/cartconvserv.go
@@ -1,21 +1,36 @@
-package test
+// Copyright 2011 Johann Höchtl. All rights reserved.
+// Use of this source code is governed by a Modified BSD License
+// that can be found in the LICENSE file.
+
+// RESTFul interface for coordinate transformations.
+package main
 
 import (
-	"github.com/the42/cartconvert"
-	"github.com/garyburd/twister/server"
-	"github.com/garyburd/twister/web"
-	"github.com/ziutek/kview"
-	"json"
+	//"github.com/the42/cartconvert"
+	//"json"
+	"http"
 	"io"
 )
 
-var layout kview.View
+const (
+	BMNHandler = "/bmn/"
+)
 
-func mainView(req *web.Request) {
-	layout.Exec(req.Respond(web.StatusOK))
+func rootHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	io.WriteString(w, "Cartography transformation")
 }
 
-func jsontest(req *web.Request) {
+func bundesmeldenetzHandler(w http.ResponseWriter, req *http.Request) {
+	// OSGB36 Datum transformation
+	// gc := cartconvert.DirectTransverseMercator(&cartconvert.PolarCoord{Latitude: flat, Longitude: flong, El: cartconvert.Airy1830Ellipsoid}, 49, -2, 0.9996012717, 400000, -100000)
+	w.Header().Set("Content-Type", "text/plain")
+	io.WriteString(w, req.URL.Path)
+
+}
+
+/*
+func (req *web.Request) {
 	w := req.Respond(web.StatusOK, web.HeaderContentType, "text/html")
 	x, err := json.Marshal(cartconvert.LatLongToGeoHash(&cartconvert.PolarCoord{Latitude: 49.3, Longitude: 20.0}))
 	if err == nil {
@@ -25,20 +40,12 @@ func jsontest(req *web.Request) {
 	}
 
 }
-
-func viewInit() {
-	// Load layout template
-	kview.TemplatesDir = "../template"
-	layout = kview.New("cartconvserverMainView.kt")
-}
+*/
 
 func main() {
-	viewInit()
 
-	router := web.NewRouter().
-		Register("/", "GET", mainView).
-		Register("/convert", "GET", jsontest)
-
-	h := web.ProcessForm(1000, false, router)
-	server.Run("localhost:1111", h)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc(BMNHandler, bundesmeldenetzHandler)
+	// TODO: Read from config file
+	http.ListenAndServe(":1111", nil)
 }
