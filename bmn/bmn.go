c408ad40131776841d43341b3a3e0f2d81bf0996
Johann Höchtl <johann.hoechtl@gmail.com>
2011-04-10 11:27:12 +0200

    remove superfluous target specification

--- bmn/bmn.go
+++ bmn/bmn.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a Modified BSD License
 // that can be found in the LICENSE file.
 
-//target:github.com/the42/cartconvert/bmn
-
 // This package provides a series of functions to deal with 
 // conversion and transformations of coordinates in the Datum Austria
 //
