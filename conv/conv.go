67712b35c57f414b88a3a918d760f1917ea056c8
Johann Höchtl <johann.hoechtl@gmail.com>
2012-02-08 18:58:31 +0000

    fix bufio.NewReader as of weekly.2012-02-07

--- conv/conv.go
+++ conv/conv.go
@@ -68,15 +68,10 @@ func main() {
 	of = ofOptions[strings.ToLower(ofcmdlinespec)]
 	ifm = ifOptions[strings.ToLower(ifcmdlinespec)]
 
-	liner, err := bufio.NewReaderSize(os.Stdin, 100)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "conv: %s (exiting)\n", err)
-		os.Exit(3)
-	}
-
+	reader := bufio.NewReaderSize(os.Stdin, 100)
 	longline := false
 
-	for data, prefix, err := liner.ReadLine(); err != io.EOF; data, prefix, err = liner.ReadLine() {
+	for data, prefix, err := reader.ReadLine(); err != io.EOF; data, prefix, err = reader.ReadLine() {
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "conv %d: %s\n", lines, err)
 			continue
