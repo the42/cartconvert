fe6b2023e1c2c26094386f5758b3ed2fe613ac85
Johann Höchtl <johann.hoechtl@gmail.com>
2012-03-03 12:28:38 +0100

    add request logging

--- cartconvserv/main.go
+++ cartconvserv/main.go
@@ -7,8 +7,34 @@
 // +build !appengine
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type logRecord struct {
+	http.ResponseWriter
+
+	time                time.Time
+	ip, method, rawpath string
+	responseBytes       int64
+	responseStatus      int
+	userAgent, referer  string
+	proto               string // "HTTP/1.1"
+}
+
+// Wrapper arround DefaultServeMux, inspired by 
+func Log(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO: add more information
+		logr := &logRecord{time: time.Now().UTC(), ip: r.RemoteAddr}
+		// TODO: make the logfile format compatible with eg. apache
+		log.Printf("%s %s", logr.time, logr.ip)
+		handler.ServeHTTP(w, r)
+	})
+}
 
 func main() {
-	http.ListenAndServe(binding(), nil)
+	http.ListenAndServe(binding(), Log(http.DefaultServeMux))
 }
